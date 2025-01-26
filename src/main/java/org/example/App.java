9b8f8728aa1058bf2715fcfa6ef2846ea70e0980
TDorin01 <timofteldorin@yahoo.com>
2025-01-26 09:08:47 +0200

    work in progress

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,14 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        FileAthlete fileAthlete = new FileAthlete();
+        fileAthlete.readFile();
+
+
+
     }
 }
